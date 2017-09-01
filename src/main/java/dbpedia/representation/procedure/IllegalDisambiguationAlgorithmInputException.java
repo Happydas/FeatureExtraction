1c73b061e417e58fbb6d994d0cfff15d77d7344c
Happydas <happy00701@gmail.com>
2017-09-01 14:16:23 +0200

    3rd

---
+++ src/main/java/dbpedia/representation/procedure/IllegalDisambiguationAlgorithmInputException.java
@@ -0,0 +1,16 @@
+package dbpedia.representation.procedure;
+
+public class IllegalDisambiguationAlgorithmInputException extends
+        IllegalArgumentException {
+
+    private static final long serialVersionUID = 1L;
+
+    IllegalDisambiguationAlgorithmInputException() {
+        super("Wrong Knowledge base!");
+    }
+
+    IllegalDisambiguationAlgorithmInputException(String text) {
+        super(text);
+    }
+
+}
